1966a9c01163a7cc03ab88992a79e34ada6b16fe
asusikov <susikov.alexander@gmail.com>
2020-10-14 20:14:28 +0300

    Refactored main

--- fibonacci/cmd/fibonacci/main.go
+++ fibonacci/cmd/fibonacci/main.go
@@ -8,15 +8,20 @@ import (
 )
 
 func main() {
-	var inputNumber = flag.Int("n", -1, "Значение для вычисления числа Фибоначчи")
-	flag.Parse()
-	fmt.Println("Высчитываем число Фибоначчи для", *inputNumber)
+  var inputNumber = parseInputNumber()
 
-	var fibonacciResult, err = fibonacci.Calculate(*inputNumber)
+	var fibonacciResult, err = fibonacci.Calculate(inputNumber)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Println("Число Фибоначчи равно", fibonacciResult)
+}
+
+func parseInputNumber() int {
+	var inputNumber = *flag.Int("n", -1, "Значение для вычисления числа Фибоначчи")
+	flag.Parse()
+	fmt.Println("Высчитываем число Фибоначчи для", inputNumber)
+  return inputNumber
 }
