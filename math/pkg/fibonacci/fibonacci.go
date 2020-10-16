909c52bf62ba5413c210fad7388515f7baa7c49e
asusikov <susikov.alexander@gmail.com>
2020-10-16 18:23:47 +0300

    Added calculation of one number instead all

--- math/pkg/fibonacci/fibonacci.go
+++ math/pkg/fibonacci/fibonacci.go
@@ -1,36 +1,10 @@
 package fibonacci
 
-const firstFibonacciElement = 1
-const secondFibonacciElement = 1
-
-func Calculate(deepSize int) []int {
-	var result []int
-	switch {
-	case deepSize < 1:
-		result = []int{}
-	case deepSize == 1:
-		result = []int{firstFibonacciElement}
+func CalculateNumber(number int) int {
+	switch number {
+	case 0, 1:
+		return number
 	default:
-		result = calculate(deepSize)
+		return CalculateNumber(number - 2) + CalculateNumber(number - 1)
 	}
-	return result
-}
-
-func calculate(deepSize int) []int {
-	return append(
-		[]int{firstFibonacciElement, secondFibonacciElement},
-		recursiveCalculate(firstFibonacciElement, secondFibonacciElement, deepSize-2)...,
-	)
-}
-
-func recursiveCalculate(first int, second int, deepLevel int) []int {
-	if deepLevel <= 0 {
-		return []int{}
-	}
-
-	newElement := first + second
-	return append(
-		[]int{newElement},
-		recursiveCalculate(second, newElement, deepLevel-1)...,
-	)
 }
--- math/pkg/fibonacci/fibonacci_test.go
+++ math/pkg/fibonacci/fibonacci_test.go
@@ -5,33 +5,9 @@ import (
 	"testing"
 )
 
-func Test_Calculate_whenInputNumberIsRight(t *testing.T) {
-	want := []int{1, 1, 2, 3, 5}
-	got := Calculate(5)
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("получили %d, ожидали %d", got, want)
-	}
-}
-
-func Test_Calculate_whenInputNumberIs1(t *testing.T) {
-	want := []int{1}
-	got := Calculate(1)
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("получили %d, ожидали %d", got, want)
-	}
-}
-
-func Test_Calculate_whenInputNumberIs2(t *testing.T) {
-	want := []int{1, 1}
-	got := Calculate(2)
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("получили %d, ожидали %d", got, want)
-	}
-}
-
-func Test_Calculate_whenInputNumberIsLess0(t *testing.T) {
-	want := []int{}
-	got := Calculate(-1)
+func Test_CalculateNumber(t *testing.T) {
+	want := 13
+	got := CalculateNumber(7)
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("получили %d, ожидали %d", got, want)
 	}
