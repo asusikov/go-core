7fcd79ce8dcd14d629116387d141843d58b8a58d
asusikov <susikov.alexander@gmail.com>
2020-10-19 19:03:12 +0300

    Added const for query

--- goondex/cmd/cli/main.go
+++ goondex/cmd/cli/main.go
@@ -10,13 +10,17 @@ import (
 
 func main() {
 	const url = "https://thinknetica.com/"
+	const query = "Гарантия"
+
 	data, err := spider.Scan(url, 3)
 	if err != nil {
 		log.Printf("ошибка при сканировании сайта %s: %v\n", url, err)
 	}
+
 	eng := engine.New()
 	eng.Index(data)
-	res := eng.Search("Гарантия")
+	res := eng.Search(query)
+
 	for k, v := range res {
 		fmt.Printf("Страница \"%s\" имеет адрес: %s\n", v, k)
 	}
