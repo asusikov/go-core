69eb614bca8dc780913cfdd232fb6472be2ab887
asusikov <susikov.alexander@gmail.com>
2020-11-12 08:33:37 +0300

    Removed new line in error

--- review/cmd/cli/main.go
+++ review/cmd/cli/main.go
@@ -43,7 +43,7 @@ func initEngine(url string) (eng *engine.Engine, err error) {
 	fmt.Println("[Сканирование] Страница -", url)
 	err = eng.Scan(url)
 	if err != nil {
-		return eng, fmt.Errorf("ошибка при сканировании сайта %s: %v\n", url, err)
+		return eng, fmt.Errorf("ошибка при сканировании сайта %s: %v", url, err)
 	}
 	return eng, nil
 }
